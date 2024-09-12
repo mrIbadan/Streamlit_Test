1118a15776d86d975c885c5f259e66f0c57c98e3
Athena <33719909+mrIbadan@users.noreply.github.com>
2024-09-12 13:57:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
 import streamlit as st
+import leafmap.foliumap as leafmap
 
-def main():
-    st.set_page_config(
-        page_title='US Risk Dashboard',
-        page_icon='✅',
-        layout='wide'
-    )
+st.set_page_config(layout="wide")
 
-    st.sidebar.title("Navigation")
-    pages = {
-        "Dashboard": dashboard,
-        "Customer Sales Trend": customer_sales_trend,
-    }
-    selection = st.sidebar.radio("Go to", list(pages.keys()))
-    pages[selection]()
+# Customize the sidebar
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
 
-def dashboard():
-    from dashboard import render_dashboard
-    render_dashboard()
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
 
-def customer_sales_trend():
-    st.title("Customer Sales Trend")
-    # Add customer sales trend content here
+# Customize page title
+st.title("Streamlit for Geospatial Applications")
 
-if __name__ == "__main__":
-    main()
+st.markdown(
+    """
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
+    """
+)
+
+st.header("Instructions")
+
+markdown = """
+1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
+2. Customize the sidebar by changing the sidebar text and logo in each Python files.
+3. Find your favorite emoji from https://emojipedia.org.
+4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+
+"""
+
+st.markdown(markdown)
+
+m = leafmap.Map(minimap_control=True)
+m.add_basemap("OpenTopoMap")
+m.to_streamlit(height=500)
