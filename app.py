4fdad04cd79ea767541461a9e2c8ebdf82b5975d
Athena <33719909+mrIbadan@users.noreply.github.com>
2024-09-12 13:05:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,3 @@ import streamlit as st
 import geopandas as gpd
 import pandas as pd
 import numpy as np
-import folium
-from folium.features import GeoJsonTooltip
-from streamlit_folium import st_folium
-import matplotlib.pyplot as plt
